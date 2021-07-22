75dec74c2e84aa011323333666f169629836a716
jorgexbermudez <jorge.bermudez.r@hotmail.com>
2021-07-23 00:26:23 +0200

    debug

--- js/myCtrl.js
+++ js/myCtrl.js
@@ -718,7 +718,7 @@ $scope.now_playing = function () {
 		  $scope.npuri [0] = response.data.item.uri;
 		  $scope.npurishort = response.data.item.uri.split(":")[2];
 		  $scope.playlistidnp = "1dxJXfnfAV46mmLrMEypIN"
-		  alert($scope.npurishort);
+		  //alert($scope.npurishort);
 		  $scope.playersrc = "https://open.spotify.com/embed/track/"+$scope.npurishort;
 
 		  $http({
